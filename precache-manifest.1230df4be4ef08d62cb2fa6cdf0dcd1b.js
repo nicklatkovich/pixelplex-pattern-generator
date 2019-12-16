768ba1bcaae0636933d1566519ed2c996c32e2fa
NickLatkovich <nicklatkovich@gmail.com>
2019-12-16 23:22:12 +0300

    Init dist

---
+++ precache-manifest.1230df4be4ef08d62cb2fa6cdf0dcd1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef4ad122d1aaa46c14b12b48c99aca7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc0a0a73071a3646ebb9",
+    "url": "/static/css/main.eff601bf.chunk.css"
+  },
+  {
+    "revision": "3915dc148aa69653cebc",
+    "url": "/static/js/2.0583f63d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0583f63d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc0a0a73071a3646ebb9",
+    "url": "/static/js/main.9b78e30c.chunk.js"
+  },
+  {
+    "revision": "c3b0cdd85cc3d529b919",
+    "url": "/static/js/runtime-main.546082e9.js"
+  }
+]);
