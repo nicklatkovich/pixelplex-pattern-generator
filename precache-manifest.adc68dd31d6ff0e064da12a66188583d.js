78bff41c6754e17a788390fc64f1557db9105b66
NickLatkovich <nicklatkovich@gmail.com>
2019-12-19 14:46:32 +0300

    Updates

---
+++ precache-manifest.adc68dd31d6ff0e064da12a66188583d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efee505695c88c2cc14c6933453b2c10",
+    "url": "/pixelplex-pattern-generator/index.html"
+  },
+  {
+    "revision": "fc0a0a73071a3646ebb9",
+    "url": "/pixelplex-pattern-generator/static/css/main.eff601bf.chunk.css"
+  },
+  {
+    "revision": "3915dc148aa69653cebc",
+    "url": "/pixelplex-pattern-generator/static/js/2.0583f63d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pixelplex-pattern-generator/static/js/2.0583f63d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc0a0a73071a3646ebb9",
+    "url": "/pixelplex-pattern-generator/static/js/main.9b78e30c.chunk.js"
+  },
+  {
+    "revision": "3706673f2a13aa683a8d",
+    "url": "/pixelplex-pattern-generator/static/js/runtime-main.4b77f884.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1230df4be4ef08d62cb2fa6cdf0dcd1b.js"
+  "/pixelplex-pattern-generator/precache-manifest.adc68dd31d6ff0e064da12a66188583d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pixelplex-pattern-generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
